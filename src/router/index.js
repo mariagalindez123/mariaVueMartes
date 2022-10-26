aefe91c12dc4f2bd8fad1b267686e913f18e5f68
mariagalindez123 <mariagalindezperafan@gmail.com>
2022-10-25 21:40:57 -0500

    creaacion archivos

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import  ArchivosView  from '../views/ArchivosView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/ArchivosView ',
+    name: 'ArchivosView ',
+    component: ArchivosView 
   },
   {
     path: '/about',
